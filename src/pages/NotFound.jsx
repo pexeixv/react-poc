0e069e37590d2c535bafa1b6c4a6cd774644357d
pexeixv <gavineditz@gmail.com>
2022-11-09 13:07:00 +0530

    404 page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <section>
+      <div className="container mx-auto max-w-7xl px-5">
+        <h1 className="text-7xl text-center font-black">Kai na re inga</h1>
+      </div>
+    </section>
+  );
+};
+export default NotFound;
